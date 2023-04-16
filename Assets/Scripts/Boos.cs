aa33b9b028a08e66d6d9f3d0e2bd6933dd51a7d7
fixnil <deping.du@gmail.com>
2023-04-17 00:01:49 +0800

    feat: 完成 boos 脚本

--- Assets/Scripts/Boos.cs
+++ Assets/Scripts/Boos.cs
@@ -1,18 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Boos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum State { Walk, Atk, Die }
+
+    public Transform character;
+    public Animator boosAnimator;
+    public int speed = 50;
+    public int range = 30;
+    public int time = 2;
+
+    private  int _hp = 100;
+    private float _atkTime;
+    private  State _state;
+
+    private void Update()
     {
-        
+        if (_hp <= 0)
+        {
+            _state = State.Die;
+        }
+
+        switch (_state)
+        {
+            case State.Walk:
+            this.Walk();
+            break;
+            case State.Atk:
+            this.Atk();
+            break;
+            case State.Die:
+            this.Die();
+            break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (_state != State.Die)
+        {
+            boosAnimator.SetTrigger("hit");
+
+            _hp--;
+
+            if (_hp <= 0)
+            {
+                _state = State.Die;
+            }
+        }
+    }
+
+    private void Walk()
+    {
+        this.transform.Rotate(speed * Time.deltaTime * Vector3.up);
+
+        var target = character.position;
+        target.y = this.transform.position.y;
+
+        var angle = Vector3.Angle(this.transform.forward, target - this.transform.position);
+
+        if (angle < range / 2)
+        {
+            _atkTime = time;
+            _state = State.Atk;
+        }
+    }
+
+    private void Atk()
+    {
+        boosAnimator.SetBool("atk", true);
+
+        _atkTime -= Time.deltaTime;
+
+        if (_atkTime <= 0)
+        {
+            _state = State.Walk;
+            boosAnimator.SetBool("atk", false);
+        }
+    }
+
+    private void Die()
+    {
+        boosAnimator.SetBool("die", true);
     }
 }
