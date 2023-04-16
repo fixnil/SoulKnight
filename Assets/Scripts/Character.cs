cd753d13b6e041aa229b4772c953e87654bf8a02
fixnil <deping.du@gmail.com>
2023-04-17 00:02:01 +0800

    feat: 完成角色脚本

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Start()
-    {
+    public int speed = 100;
+    public CharacterController characterController;
+    public Animator characterAnimator;
 
-    }
-
-    // Update is called once per frame
     private void Update()
     {
+        this.Move();
+        this.Rotate();
+        this.Atk();
+    }
 
+    private void Move()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            characterAnimator.SetBool("walk", true);
+
+            var direction = new Vector3(horizontal, 0, vertical);
+            characterController.SimpleMove(direction * speed);
+        }
+        else
+        {
+            characterAnimator.SetBool("walk", false);
+        }
+    }
+
+    private void Rotate()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit, 1 << 6))
+        {
+            var target = hit.point;
+            target.y = this.transform.position.y;
+            this.transform.LookAt(target);
+        }
+    }
+
+    private void Atk()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            characterAnimator.SetBool("atk", true);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            characterAnimator.SetBool("atk", false);
+        }
     }
 }
